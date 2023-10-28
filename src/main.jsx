7f39558d47b80935a4f1599f449a1be1a125ec7a
Kevin Guzmán <guzmankevin420@gmail.com>
2023-10-28 10:55:03 -0600

    enable the api url

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import { NextUIProvider } from "@nextui-org/react";
 import App from "./App.jsx";
 import "./index.css";
 
-axios.defaults.baseURL = 'http://localhost:5000'
-// axios.defaults.baseURL = 'https://servertest-7be1e94c801c.herokuapp.com/'
+// axios.defaults.baseURL = 'http://localhost:5000'
+axios.defaults.baseURL = 'https://servertest-7be1e94c801c.herokuapp.com/'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
